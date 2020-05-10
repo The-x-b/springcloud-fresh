a5cc8fe1743f885802c540578a550d9258dcf844
xiao <2543016355@qq.com>
2020-05-10 15:56:51 +0800

    添加配置中心

---
+++ springcloud-fresh-config/src/test/java/com/ac/springcloud/fresh/config/AppTest.java
@@ -0,0 +1,20 @@
+package com.ac.springcloud.fresh.config;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
